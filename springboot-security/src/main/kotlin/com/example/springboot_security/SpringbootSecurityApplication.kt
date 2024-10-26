edc9adb27a2fcf67380f5b0d71fa8ebae8c63766
jaeseok <jaeseok0515@gmail.com>
2024-10-26 18:02:31 +0900

    project 생성

---
+++ springboot-security/src/main/kotlin/com/example/springboot_security/SpringbootSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboot_security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootSecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootSecurityApplication>(*args)
+}
